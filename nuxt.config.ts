177f723e82615c69c11ae8940126a70787391fbd
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-18 14:42:17 +0530

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,6 @@
-import { defineNuxtConfig } from 'nuxt3';
+import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  head: {
-    title: 'layer0-nuxt3-example',
-    link: [
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Lato:400,700',
-      },
-    ],
-  },
-});
+  modules: ['@nuxtjs/tailwindcss'],
+})
