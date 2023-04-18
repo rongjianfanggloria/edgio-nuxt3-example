202d69a25cd3c61756b750344e39bb05b2bbd9f5
Gloria <grong@edg.io>
2023-04-18 16:43:01 +0800

    modfication in caching and headers

--- routes.ts
+++ routes.ts
@@ -1,17 +1,8 @@
 import { Router } from '@edgio/core'
 import { nuxtRoutes } from '@edgio/nuxt-nitro'
 // export default new Router().match("/", ({ cache }) => { cache({ edge: { maxAgeSeconds: 60 * 60 * 24, staleWhileRevalidateSeconds: 60 * 60 } }) }).use(nuxtRoutes);
-
 export default new Router()
-    .match({}, {
-        'headers': {
-            set_response_header: {
-                "x-gloria-cookie": "%{cookie_<COOKIE>}",
-                "x-request-uri": "%{request_uri}"
 
-            }
-        }
-    })
     // .get('/doc', {
     //     'response': {
     //         'set_response_body': 'Hello GloriaTest2304',
@@ -28,7 +19,15 @@ export default new Router()
     //         }
     //     }
     // })
-    .match("/", ({ cache }) => { cache({ edge: { maxAgeSeconds: 60 * 60 * 24, staleWhileRevalidateSeconds: 60 * 60 } }) })
+    .match("/", ({ cache }) => { cache({ edge: { maxAgeSeconds: 60 * 60 * 24, staleWhileRevalidateSeconds: 60 * 60 }}) })
+    .match("/",  {
+        'headers': {
+            set_response_header: {
+                "x-gloria-cookie": "%{cookie_<COOKIE>}",
+                "x-request-uri": "%{request_uri}"
+
+            }
+        }})
     .use(nuxtRoutes);
 
 
