ade9b5ac3c12c579395dc6a25740d26817c67fa4
Gloria <grong@edg.io>
2023-04-18 16:16:27 +0800

    adding caching rules

--- routes.ts
+++ routes.ts
@@ -3,31 +3,32 @@ import { nuxtRoutes } from '@edgio/nuxt-nitro'
 // export default new Router().match("/", ({ cache }) => { cache({ edge: { maxAgeSeconds: 60 * 60 * 24, staleWhileRevalidateSeconds: 60 * 60 } }) }).use(nuxtRoutes);
 
 export default new Router()
-.match({}, {
-    'headers': {
-      set_response_header: { 
-        "x-gloria-cookie": "%{cookie_<COOKIE>}",
-        "x-request-uri": "%{request_uri}"
-  
-      }
-    }
-  })
-  .get('/doc', {
-    'response': {
-      'set_response_body': 'Hello GloriaTest2304',
-      'set_done': true
-    }
-  })
-  .get('/docs/guide/concepts/:id', {
-    caching: { max_age: { 200: "3600s" }, stale_while_revalidate: "3600s", bypass_client_cache: true },
-  })
-  .match({}, {
-    'headers': {
-      set_response_header: { 
-        'original-request-path-Gloria': '%{path}' 
-      }
-    }
-  })
-  .use(nuxtRoutes);
+    .match({}, {
+        'headers': {
+            set_response_header: {
+                "x-gloria-cookie": "%{cookie_<COOKIE>}",
+                "x-request-uri": "%{request_uri}"
+
+            }
+        }
+    })
+    .get('/doc', {
+        'response': {
+            'set_response_body': 'Hello GloriaTest2304',
+            'set_done': true
+        }
+    })
+    .get('/docs/guide/concepts/:id', {
+        caching: { max_age: { 200: "3600s" }, stale_while_revalidate: "3600s", bypass_client_cache: true },
+    })
+    .match({}, {
+        'headers': {
+            set_response_header: {
+                'original-request-path-Gloria': '%{path}'
+            }
+        }
+    })
+    .match("/", ({ cache }) => { cache({ edge: { maxAgeSeconds: 60 * 60 * 24, staleWhileRevalidateSeconds: 60 * 60 } }) })
+    .use(nuxtRoutes);
 
 
