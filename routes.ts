38a47a7ecdd5171256d59efc04e4557dae41757e
Gloria <grong@edg.io>
2023-04-18 13:39:06 +0800

    adding two more testPages

--- routes.ts
+++ routes.ts
@@ -1,6 +1,6 @@
 import { Router } from '@edgio/core'
 import { nuxtRoutes } from '@edgio/nuxt-nitro'
-import { CustomCacheKey } from '@edgio/core/router'
+// import { CustomCacheKey } from '@edgio/core/router'
 export default new Router().match("/", ({ cache }) => { cache({ edge: { maxAgeSeconds: 60 * 60 * 24, staleWhileRevalidateSeconds: 60 * 60 } }) }).use(nuxtRoutes);
 
 
